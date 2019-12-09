8e63878d8b750ab4209d79a20e52f695f4e3a572
flyq <flyq951@gmail.com>
2019-12-09 07:24:35 -0800

    add

---
+++ address/myaddr/target/debug/deps/myaddr-6a3df1649302616d.d
@@ -0,0 +1,5 @@
+/home/flyq/workspaces/flyq/blogs/address/myaddr/target/debug/deps/myaddr-6a3df1649302616d: src/main.rs
+
+/home/flyq/workspaces/flyq/blogs/address/myaddr/target/debug/deps/myaddr-6a3df1649302616d.d: src/main.rs
+
+src/main.rs:
---
+++ address/myaddr/target/debug/myaddr.d
@@ -0,0 +1,1 @@
+/home/flyq/workspaces/flyq/blogs/address/myaddr/target/debug/myaddr: /home/flyq/workspaces/flyq/blogs/address/myaddr/src/main.rs
